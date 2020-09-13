f732abff1e91e191bf36838b1b08e75603c2d2cf
ELOISA\Eloisa <eloisamedrado_11@hotmail.com>
2020-09-13 19:06:06 -0300

    Adding content on the About page. Moving object from Main page to About page. Adding date and time of the last update on the pages.

--- src/About.js
+++ src/About.js
@@ -4,17 +4,30 @@ import {useHistory} from "react-router-dom";
 
 function About(props) {
   const history = useHistory();
+  const {state} = props.location;
 
   function goBackMain() {
     history.push("/");
   };
 
+  if(!state || !state.sols) {
+    goBackMain();
+    return null;
+  }
+  const {sols} = state;
+  let {lastUpdate} = state;
+
   return (
     <div className="About">
       <div className="InnerAbout">
+        <p className="border">Sol: {sols.sol}</p>
+        <p className="border">Minimun: {sols.low}</p>
+        <p className="border">Average: {sols.average}</p>
+        <p className="border">High: {sols.high}</p>
 
         <button onClick={goBackMain}>Go back</button>
-
+        <p>Last Update: {lastUpdate}</p>
+        <p>*Updated every 10 minutes*</p>
       </div>
     </div>
   );
--- src/Main.js
+++ src/Main.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from 'react';
 import './General.css';
 import DataTable from 'react-data-table-component';
 import {useHistory} from "react-router-dom";
+import * as Utils from './Utils';
 
-const data = [{"sol":635,"low":-95.169,"high":-16.453,"average":-62.311},
-  {"sol":636,"low":-94.58,"high":-17.111,"average":-61.522},
-  {"sol":638,"low":-94.696,"high":-14.912,"average":-61.625}];
+const data = {"availableSols":[{"sol":635,"low":-95.169,"high":-16.453,"average":-62.311},
+    {"sol":636,"low":-94.58,"high":-17.111,"average":-61.522},
+    {"sol":638,"low":-94.696,"high":-14.912,"average":-61.625}],"lastUpdate":"2020-09-13T22:02:05.742+00:00"};
 
 const columns = [
   {
@@ -22,9 +23,15 @@ const columns = [
 ];
 
 function Main() {
+  let lastUpdateDate = "";
+  if(data && data.lastUpdate) {
+    let date = new Date(data.lastUpdate);
+    lastUpdateDate = Utils.formatDate(date);
+  }
+
   const history = useHistory();
-  function openSolAbout() {
-    history.push('/about');
+  function openSolAbout(e) {
+    history.push('/about', {sols: e, lastUpdate: lastUpdateDate});
   };
 
   return (
@@ -33,9 +40,11 @@ function Main() {
         <DataTable
           title="Avaliable Sols"
           columns={columns}
-          data={data}
+          data={data.availableSols}
           onRowDoubleClicked={openSolAbout}
         />
+        <p>Last Update: {lastUpdateDate}</p>
+        <p>*Updated every 10 minutes*</p>
       </div>
     </div>
   );
